372d6ee309ab2a463a2317738257897464fd6e26
Tanvi-rk260 <kocharekartanvi5@gmail.com>
2025-06-04 17:40:26 +0530

    chnges done broken link checker tool

---
+++ src/app/brokenStreamForm/page.jsx
@@ -0,0 +1,12 @@
+import BrokenStreamForm from '@/components/brokenStreamForm/brokenStreamForm'
+import Navbar from '@/components/layout/navbar'
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div>
+      <Navbar/>
+      <BrokenStreamForm/>
+    </div>
+  )
+}
