e3c6f409e98b21baa47face681a353792c00b0ba
Noor Ali <shaikhnoorali92@gmail.com>
2025-07-12 07:33:46 +0530

    send

--- src/app/john-the-ripper/page.jsx
+++ src/app/john-the-ripper/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
-import JohnRipper from "@/components/john/JohnRipper";
+import johnRipper from "@/components/john/JohnRipper";
 
 export default function JohnTheRipperPage() {
   return (
