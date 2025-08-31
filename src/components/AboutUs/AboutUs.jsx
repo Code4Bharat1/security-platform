b304d0fa04711a1e9466fd53d290ad1caeae78cc
Faraaz Kurawle <kurawlefaraaz@gmail.com>
2025-08-31 20:22:38 +0530

    remove nav and footer import in aboutus page

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,16 +1,12 @@
-import Navbar from './Navbar'
 import Branches from './Branches'
 import Specialiazation from './Specialization'
 import Services from './Services'
-import Footer from './Footer'
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
       <Branches></Branches>
       <Specialiazation></Specialiazation>
       <Services></Services>
-      <Footer></Footer>
     </div>
   )
 }
