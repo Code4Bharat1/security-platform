a9c78b5e71ffc9918c3f8dac669227b09cd680eb
Faraaz Kurawle <kurawlefaraaz@gmail.com>
2025-08-31 20:24:39 +0530

    remove services section in about us page

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,12 +1,10 @@
 import Branches from './Branches'
 import Specialiazation from './Specialization'
-import Services from './Services'
 function App() {
   return (
     <div>
       <Branches></Branches>
       <Specialiazation></Specialiazation>
-      <Services></Services>
     </div>
   )
 }
