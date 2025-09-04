38725b6e60823e467af9c75b1d5461027e0c2407
Faheem Daruwala <faheemdaruwala1@gmail.com>
2025-09-04 16:10:03 +0530

    feat: add framer-motion dependency and update service components with new images and functionality

--- src/components/Services/Layout.jsx
+++ src/components/Services/Layout.jsx
@@ -18,7 +18,7 @@ function DescHero({ data }) {
                 muted
                 playsInline
             >
-                <source src="/AV.mp4" type="video/mp4" />
+                <source src="/CS.mov" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             {/* Content */}
