4929f8b1be1805745b76f0e20f4a1af8c3aa2d8b
abdulahadkhan-10 <st.abdul24@gmail.com>
2025-09-05 11:32:16 +0530

    changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 import Home from "@/components/Home/Home";
 
 export default function Landing() {
