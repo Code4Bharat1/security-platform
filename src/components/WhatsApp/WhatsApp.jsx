e8e1ebf6352abd111d513ca93e0b0a3b6c9a2c9e
kg8838166@gmail.com <kg8838166@gmail.com>
2025-09-16 12:34:49 +0530

    Add WhatsApp component and update styles for better positioning

--- src/components/WhatsApp/WhatsApp.jsx
+++ src/components/WhatsApp/WhatsApp.jsx
@@ -7,11 +7,11 @@ function WhatsApp() {
       href="https://wa.me/919594430295" 
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-20 right-5 z-50"
+      className="fixed bottom-15 right-10 z-50"
     >
-      <FaWhatsapp 
-        className="w-16 h-16 text-green-500 hover:scale-110 transition-transform duration-300 bg-white rounded-full shadow-md p-2"
-      />
+      <div className="w-16 h-16 bg-[#25D366] rounded-full flex items-center justify-center  shadow-lg hover:scale-110 transition-transform duration-300">
+        <FaWhatsapp className="text-white w-20 h-20" />
+      </div>
     </a>
   );
 }
